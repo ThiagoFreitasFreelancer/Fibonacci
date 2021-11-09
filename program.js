8bad81b7125ee1aea1682b43035967ca0c560495
Thiago Freitas <tfgoncalvesfreelancer@gmail.com>
2021-11-09 11:21:25 -0300

    update

---
+++ program.js
@@ -0,0 +1,7 @@
+
+let n = 4
+const a = 5
+
+let result2 = (((((1 + Math.sqrt(a))) / 2) ** (n + 1)) - ((((1 - Math.sqrt(a))) / 2) ** (n + 1))) / Math.sqrt(a)
+
+console.log("Reslt 2:", result2.toFixed(0))
